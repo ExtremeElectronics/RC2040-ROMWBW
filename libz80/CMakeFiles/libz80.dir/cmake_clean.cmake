e9f0dfd08d4950e298de3c710b7f0e755e1a0a4d
Extreme Electronics <102665314+ExtremeElectronics@users.noreply.github.com>
2025-08-04 19:40:48 +0100

    Libz80

---
+++ libz80/CMakeFiles/libz80.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/libz80.dir/z80.c.obj"
+  "liblibz80.a"
+  "liblibz80.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/libz80.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ libz80/CMakeFiles/libz80.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "liblibz80.a"
+)
